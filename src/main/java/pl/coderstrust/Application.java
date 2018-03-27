33075f0035ce2acc0c81f495dcdd5ea649671e5f
Bartek S <kursantjava68@gmail.com>
2018-03-27 22:49:46 +0200

    pom.xml dependence's and @ComponentScan from Application deleting.

--- src/main/java/pl/coderstrust/Application.java
+++ src/main/java/pl/coderstrust/Application.java
@@ -2,10 +2,8 @@ package pl.coderstrust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"processing", "controller", "persistence", "config"})
 public class Application {
 
   public static void main(String[] args) {
