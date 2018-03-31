019b1c04d2d9442d470904a29d8cfce01b5ff7e9
Bartek Slabiak <kursantjava68@gmail.com>
2018-03-31 17:55:32 +0200

    New class.

---
+++ src/main/java/pl/coderstrust/config/InvoiceRequestInterceptorConfig.java
@@ -0,0 +1,24 @@
+package pl.coderstrust.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+    @EnableWebMvc
+    @Configuration
+    public class InvoiceRequestInterceptorConfig extends WebMvcConfigurerAdapter {
+
+        @Bean
+        public InvoiceRequestInterceptor invoiceRequestInterceptor(){
+            return new InvoiceRequestInterceptor();
+        }
+
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(invoiceRequestInterceptor())
+                    .addPathPatterns("/**");
+        }
+    }
+}
