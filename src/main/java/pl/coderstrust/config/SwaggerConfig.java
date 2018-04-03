95156681cdbc8b578dcadaf8ae001b0cd7ae7e19
Bartek Slabiak <kursantjava68@gmail.com>
2018-04-03 20:45:54 +0200

    Added ApiInfo

--- src/main/java/pl/coderstrust/config/SwaggerConfig.java
+++ src/main/java/pl/coderstrust/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package pl.coderstrust.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,9 +17,22 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Invoice Simple", "http://github.com", "kursantjava68@gmail.com");
+        return new ApiInfoBuilder()
+                .title("Invoice Simple")
+                .description("Here is the best app for making an invoice, ")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
                 .build();
     }
 }
